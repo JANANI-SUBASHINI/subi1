30893d3164b5738574670cfe2970f7fa66b8cd64
JANANI-SUBASHINI <janani.jananir.r@gmail.com>
2017-07-08 15:01:05 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+# subi1
+import java.io.*;
+import java.util.Scanner;
+public class CamelCase1
+ {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a sentence: ");
+    String input = sc.nextLine();
+    String r = "";
+    char a = input.charAt(0);
+    r = r + Character.toUpperCase(a);
+    for (int i = 1; i < input.length(); i++) {
+      char b = input.charAt(i);
+      char c = input.charAt(i - 1);
+      if (c == ' ') {
+        r = r + Character.toUpperCase(b);
+      } else {
+        r = r + b;
+      }
+    }
+    System.out.println(r);
+  }
+}
